9df50ebb973dd326ce490b185f1bf0980f696792
Jiang Haifeng <jianghaifeng@jiagouyun.com>
2019-04-22 14:06:44 +0800

    两数相加 添加注释

--- src/leetcode/list/leet_zh_2/Solution.java
+++ src/leetcode/list/leet_zh_2/Solution.java
@@ -9,6 +9,8 @@ public class Solution {
      * 执行用时 : 10 ms, 在Add Two Numbers的Java提交中击败了92.72% 的用户
      * 内存消耗 : 45.1 MB, 在Add Two Numbers的Java提交中击败了81.36% 的用户
      *
+     * 空间复杂度 O(1)，+1 直到末尾还需进位则考虑需要 new ListNode(carry)，节点
+     * 时间复杂度 O(n)
      * 写的有些繁琐，待考虑是否可优化，先睡午觉去。
      * */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
@@ -16,12 +18,20 @@ public class Solution {
             return l2;
         if (l2 == null)
             return l1;
+
+        // 两个虚拟头结点
         ListNode dummyHead1 = new ListNode(-1);
         dummyHead1.next = l1;
         ListNode dummyHead2 = new ListNode(-1);
         dummyHead2.next = l2;
+
+        // 进位
         int carry = 0;
+        // 基值
         int base = 10;
+
+        // 以下注释基于 demo: l1=[1,2,6] l2=[4,5,6,9]
+        // l1=[1,2,6] l2=[4,5,6,9] -> l1=[5,7,2] carry=1
         while (dummyHead1.next != null) {
             dummyHead1 = dummyHead1.next;
 
@@ -41,9 +51,12 @@ public class Solution {
             dummyHead1.val = val;
         }
 
+        // l1=[5,7,2] dummyHead1.next=[] dummyHead2.next=[9] carry=1 -> dummyHead1.next=[9] l1=[5,7,2,9] carry=1
         if (dummyHead2.next != null) {
             dummyHead1.next = dummyHead2.next;
         }
+
+        //  dummyHead1.next=[9] l1=[5,7,2,9] carry=1 -> l1=[5,7,2,0,1] carry=0
         while (carry != 0) {
             if (dummyHead1.next == null) {
                 dummyHead1.next = new ListNode(0);
