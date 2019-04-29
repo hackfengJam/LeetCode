ffaacbe80a7f73190cdf220209d17a94954bf49a
Jiang Haifeng <jianghaifeng@jiagouyun.com>
2019-04-29 17:59:09 +0800

    不想交的线 移动石子直到连续

--- src/weekly_contest/th_134/leet_zh_5039/Solution.java
+++ src/weekly_contest/th_134/leet_zh_5039/Solution.java
@@ -8,18 +8,37 @@ public class Solution {
      * */
     public int[] numMovesStones(int a, int b, int c) {
         int[] ans = new int[2];
+        if (a > b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        if (b > c) {
+            int tmp = b;
+            b = c;
+            c = tmp;
+        }
+        if (a > b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
 
         int left = b - a - 1;
         int right = c - b - 1;
-        if (left < 0)
-            left = 0;
-        if (right < 0)
-            right = 0;
 
-        ans[0] = left + right;
+        ans[0] = 2;
         ans[1] = left + right;
+        if (right == 0 && left == 0) ans[0] = 0;
+        else if (right <= 1 || left <= 1) ans[0] = 1;
 
 
         return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution ss = new Solution();
+        int[] ans = ss.numMovesStones(4, 2, 3);
+        System.out.println(ans[0] + " " + ans[1]);
     }
 }
--- src/weekly_contest/th_134/leet_zh_5041/Solution.java
+++ src/weekly_contest/th_134/leet_zh_5041/Solution.java
@@ -4,10 +4,51 @@ public class Solution {
     /*
      * 5041. 不相交的线
      *
-     *
+     * 动态规划，最长子序列
+     * ans[i][j] =
+     *   ans[i-1][j-1]+1                    (A[i] = B[j])
+     *   max(ans[i][j-1], ans[i-1][j])  (A[i] != B[j])
      * */
     public int maxUncrossedLines(int[] A, int[] B) {
+        int[][] ans = new int[A.length][B.length];
+        ans[0][0] = A[0] == B[0] ? 1 : 0;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] == B[0])
+                ans[i][0] = 1;
+            else
+                ans[i][0] = ans[i - 1][0];
+        }
+        for (int j = 1; j < B.length; j++) {
+            if (B[j] == A[0])
+                ans[0][j] = 1;
+            else
+                ans[0][j] = ans[0][j - 1];
+        }
 
-        return 0;
+        for (int i = 1; i < A.length; i++) {
+            for (int j = 1; j < B.length; j++) {
+                if (A[i] == B[j]) {
+                    ans[i][j] = ans[i - 1][j - 1] + 1;
+                } else {
+                    ans[i][j] = Math.max(ans[i][j - 1], ans[i - 1][j]);
+                }
+            }
+        }
+
+        return ans[A.length - 1][B.length - 1];
+    }
+
+    public static void main(String[] args) {
+        Solution ss = new Solution();
+//        int[] A = {1, 4, 2};
+//        int[] B = {1, 2, 4};
+
+//        int[] A = {2, 5, 1, 2, 5};
+//        int[] B = {10, 5, 2, 1, 5, 2};
+        int[] A = {1, 3, 7, 1, 7, 5};
+        int[] B = {1, 9, 2, 5, 1};
+        int ans = ss.maxUncrossedLines(A, B);
+        System.out.println(ans);
+
     }
 }
