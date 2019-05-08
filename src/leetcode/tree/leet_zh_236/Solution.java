ea3304a663c0536069a10dd00bc966b87fd85056
Jiang Haifeng <jianghaifeng@jiagouyun.com>
2019-05-08 18:02:19 +0800

    236. 二叉树的最近公共祖先

--- src/leetcode/tree/leet_zh_236/Solution.java
+++ src/leetcode/tree/leet_zh_236/Solution.java
@@ -8,53 +8,24 @@ public class Solution {
      * 236. 二叉树的最近公共祖先
      *
      *
-     * 执行用时 : 9 ms, 在Lowest Common Ancestor of a Binary Tree的Java提交中击败了96.17% 的用户
-     * 内存消耗 : 34.5 MB, 在Lowest Common Ancestor of a Binary Tree的Java提交中击败了84.61% 的用户
+     * 执行用时 : 16 ms, 在Lowest Common Ancestor of a Binary Tree的Java提交中击败了50.12% 的用户
+     * 内存消耗 : 39.1 MB, 在Lowest Common Ancestor of a Binary Tree的Java提交中击败了5.74% 的用户
      * */
-    private TreeNode ret;
-    private TreeNode findP;
-    private TreeNode findQ;
-
-    private TreeNode lowest(TreeNode node, TreeNode p, TreeNode q) {
-        if (node == null)
-            return null;
-        if (ret != null)
-            return ret;
-
-        boolean isF = false;
-
-        // 进入时已经找到过 p 或 q，那么本节点不会是p、q父亲节点
-        if (findP != null || findQ != null) {
-            isF = true;
-        }
-
-        ret = lowest(node.left, p, q);
-        if (ret != null)
-            return ret;
-        ret = lowest(node.right, p, q);
-        if (ret != null)
-            return ret;
-
-        if (p.val == node.val) {
-            findP = node;
-        }
-        if (q.val == node.val) {
-            findQ = node;
-        }
-
-        // 进入时已经找到过 p 或 q，那么本节点不会是p、q父亲节点
-        if (!isF && findP != null && findQ != null)
-            return node;
-
-        return null;
-    }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ret = null;
-        findP = null;
-        findQ = null;
-        ret = lowest(root, p, q);
-        return ret;
+        if (root == null)
+            return null;
+        if (root.val == p.val || root.val == q.val)
+            return root;
+        TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
+        if (leftNode != null && rightNode != null)
+            return root;
+        else if (leftNode != null)
+            return leftNode;
+        else if (rightNode != null)
+            return rightNode;
+        return null;
     }
 
     public static void main(String[] args) {
