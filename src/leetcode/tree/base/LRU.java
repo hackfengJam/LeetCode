6459bee8d48d5ca552ba63c89085d8501cd51c5a
Jiang Haifeng <jianghaifeng@jiagouyun.com>
2019-06-18 11:51:51 +0800

    LRU

--- src/leetcode/tree/base/LRU.java
+++ src/leetcode/tree/base/LRU.java
@@ -1,5 +1,8 @@
 package leetcode.tree.base;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LRU {
     /*
     *
@@ -10,6 +13,9 @@ public class LRU {
     使用数据时候将数据移动到头结点。
     缓存数量超过阈值时移除链表尾部数据。
     * */
+
+
+    private Map<Integer, TreeNode> cache = new HashMap<>();
 
     /**
      * 最大缓存大小
@@ -30,8 +36,8 @@ public class LRU {
     /**
      * 尾节点
      */
-//    private Node<K,V> tailer;
     private TreeNode tailer;
+//    private Node<K,V> tailer;
 
     public LRU(int cacheSize) {
         this.cacheSize = cacheSize;
@@ -55,4 +61,62 @@ public class LRU {
         tailer.left = header;
     }
 
+    public int get(int key) {
+        TreeNode node = cache.get(key);
+        if (node == null) {
+            // should raise exception here.
+            return -1;
+        }
+
+        // 当前访问节点移动到头部
+        this.moveToHead(node);
+
+        return node.val;
+    }
+
+
+    /**
+     * 总是向 header 右边添加新的 node
+     */
+    private void addNode(TreeNode node) {
+        if (nodeCount + 1 > cacheSize)
+            // TODO 待优化
+            popTail();
+
+        node.left = header;
+        node.right = header.right;
+
+        header.right = node;
+        header.right.left = node;
+
+        nodeCount++;
+    }
+
+    /**
+     * 从链表中移除一个存在的 node
+     */
+    private void removeNode(TreeNode node) {
+        TreeNode pre = node.left;
+        TreeNode post = node.right;
+
+        pre.right = post;
+        post.left = pre;
+    }
+
+    /**
+     * 将 node 从中间移到头部
+     */
+    private void moveToHead(TreeNode node) {
+        this.removeNode(node);
+        this.addNode(node);
+    }
+
+    /**
+     * pop 当前尾部元素
+     */
+    private TreeNode popTail() {
+        TreeNode res = tailer.left;
+        this.removeNode(res);
+        return res;
+    }
 }
