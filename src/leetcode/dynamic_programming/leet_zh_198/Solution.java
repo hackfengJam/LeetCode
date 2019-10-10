be9780e64898811321842c7c0cfc05316a07feb2
vesper <hackfunjiang@163.com>
2019-10-10 17:59:23 +0800

    fix bug

--- src/leetcode/dynamic_programming/leet_zh_198/Solution.java
+++ src/leetcode/dynamic_programming/leet_zh_198/Solution.java
@@ -3,20 +3,17 @@ package leetcode.dynamic_programming.leet_zh_198;
 public class Solution {
     /*
      * 198. 打家劫舍
-     * 执行用时 : 2 ms, 在House Robber的Java提交中击败了60.13% 的用户
-     * 内存消耗 : 34.4 MB, 在House Robber的Java提交中击败了84.46% 的用户
+     * 执行用时 : 1 ms, 在所有 Java 提交中击败了82.21% 的用户
+     * 内存消耗 : 33.9 MB, 在所有 Java 提交中击败了90.44% 的用户
      * */
     public int rob(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
-//        if (nums.length == 1) return nums[0];
 
-        int ans[] = new int[nums.length];
+        int[] ans = new int[nums.length];
 
         ans[0] = nums[0];
-//        ans[1] = Math.max(nums[0], nums[1]);
 
-//        for (int i = 1; i < nums.length; i++) {
-        for (int i = 2; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++) {
             int max = Math.max(ans[i - 1], nums[i]);
             for (int j = 0; j < i - 1; j++) {
                 int val = ans[j] + nums[i];
